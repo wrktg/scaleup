9c5f824fa09c4210e2b4ce9d0e843f4713220080
Taras Mankovski <tarasm@gmail.com>
2013-01-30 03:09:32 +0100

    Added get_value method to ScaleUp_Schema_Property

--- classes/class-schema-property.php
+++ classes/class-schema-property.php
@@ -1,17 +1,47 @@
 <?php
 class ScaleUp_Schema_Property extends ScaleUp_Base {
 
-  function __construct( $property_name, $args = null ) {
+  function __construct( $property_name, $args = array() ) {
 
-    $default = array(
-      'name'        => $property_name,
-      'meta_type'   => 'post',
-      'data_type'   => 'Text',
-    );
+    $default = array();
+
+    $schema_property = ScaleUp_Schemas::get_property( $property_name );
+    if ( is_array( $schema_property ) )
+      $default = $schema_property;
+    else
+      $this->_error = new WP_Error( 'instantiation', sprintf( __( '%s schema property does not exist.' ), $property_name ) );
+
+    $default[ 'name' ]      = $property_name;
+    $default[ 'meta_type' ] = 'post';
 
     $args = wp_parse_args( $args, $default );
 
     parent::__construct( $args );
+  }
+
+  /**
+   * Return value in this property
+   * 
+   * @return bool|float|int|null
+   */
+  function get_value() {
+
+    if ( !isset( $this->_value ) )
+      return null;
+    else
+      $value = $this->_value;
+
+    $ranges = $this->get( 'ranges' );
+    if ( in_array( "Integer", $ranges ) )
+      return (int) $value;
+
+    if ( in_array( "Boolean", $ranges ) )
+      return (bool) $value;
+
+    if ( in_array( "Float", $ranges ) )
+    return (float) $value;
+
+    return $value;
   }
 
   /**
