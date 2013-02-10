e30df2e33c5d1d30ea36c2abacbc85e42b8bb730
Taras Mankovski <tarasm@gmail.com>
2013-02-10 13:55:55 +0100

    Modified includes to load after plugins_loaded

--- scaleup.php
+++ scaleup.php
@@ -10,21 +10,25 @@ define( 'SCALEUP_MIN_WP',  '3.4' );
 /**
  * Base classes
  */
-require_once( SCALEUP_DIR . '/classes/class-scaleup.php' );
 require_once( SCALEUP_DIR . '/classes/class-base.php' );
 require_once( SCALEUP_DIR . '/classes/class-contextual.php' );
+require_once( SCALEUP_DIR . '/classes/class-scaleup.php' );
 
-include_once( SCALEUP_DIR . '/classes/class-form.php' );
-include_once( SCALEUP_DIR . '/classes/class-forms.php' );
-include_once( SCALEUP_DIR . '/classes/class-form-field.php' );
-include_once( SCALEUP_DIR . '/classes/class-schemas.php' );
-include_once( SCALEUP_DIR . '/classes/class-schema-property.php' );
-include_once( SCALEUP_DIR . '/classes/class-schema-type.php' );
-include_once( SCALEUP_DIR . '/classes/class-schema.php' );
-include_once( SCALEUP_DIR . '/classes/class-templates.php' );
 include_once( SCALEUP_DIR . '/functions.php' );
 include_once( SCALEUP_DIR . '/template_tags.php' );
 
-new ScaleUp_Templates();
-new ScaleUp_Schemas();
-new ScaleUp_Forms();
+function scaleup_plugins_loaded() {
+  include_once( SCALEUP_DIR . '/classes/class-form.php' );
+  include_once( SCALEUP_DIR . '/classes/class-forms.php' );
+  include_once( SCALEUP_DIR . '/classes/class-form-field.php' );
+  include_once( SCALEUP_DIR . '/classes/class-schemas.php' );
+  include_once( SCALEUP_DIR . '/classes/class-schema-property.php' );
+  include_once( SCALEUP_DIR . '/classes/class-schema-type.php' );
+  include_once( SCALEUP_DIR . '/classes/class-schema.php' );
+  include_once( SCALEUP_DIR . '/classes/class-templates.php' );
+
+  new ScaleUp_Templates();
+  new ScaleUp_Schemas();
+  new ScaleUp_Forms();
+}
+add_action( 'plugins_loaded', 'scaleup_plugins_loaded' );
